b2c827f47e8451df7132e7dfec4a928b94c25936
Joel Jr <joelaraujojuca10@gmail.com>
2024-10-17 11:04:31 -0300

    Criação Projeto

---
+++ src/main/java/com/example/SGP/SgpApplication.java
@@ -0,0 +1,13 @@
+package com.example.SGP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SGP/SgpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SGP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
